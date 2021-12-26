e0646cfc174346d1123be30e92cd88eb7a475913
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-26 03:02:58 -0600

    [Development] - Update

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ const AWS = XRay.captureAWS(require("aws-sdk"));
 const Layer = XRay.captureAWS(require("@cloud-vault/http-responses-lambda-layer"));
 
 // Create Client outside of Handler for reuse
-const Lambda = new AWS.Lambda();
+// const Lambda = new AWS.Lambda();
 
-/*** Handler (Async Handler(s) *should* return a non-awaited promise */
+/*** Handler (Async Handler(s) *should* return a non-awaited promise) */
 exports.handler = async function (event, context) {
     console.log("[Debug] Environment Variable(s)" + ":", serialize(process.env));
     console.log("[Debug] Lambda Handler Context" + ":", serialize(context));
     console.log("[Debug] Event" + ":", serialize(event));
 
     return serialize({
-        message: "Hello World"
+        Layer
     });
 };
 
-// Use SDK Client
-const getAccountSettings = function () {
-    return Lambda.getAccountSettings().promise();
-};
+//// Use SDK Client
+//const getAccountSettings = function () {
+//    return Lambda.getAccountSettings().promise();
+//};
 
 const serialize = (object) => JSON.stringify(object, null, 4);
